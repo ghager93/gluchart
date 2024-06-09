4f9c40b5824af539e7e6057cf19f0bb7bc8b28da
Gerard Hager <ghager93@gmail.com>
2024-06-10 00:49:44 +1000

    htmx graph showing up (wrongly)

--- values/views.py
+++ values/views.py
@@ -1,3 +1,4 @@
+import json
 import math
 from datetime import timedelta
 
@@ -53,6 +54,7 @@ def htmx_component(request):
     template_name = 'htmx_component.html'
     return render(request, template_name)
 
+
 def htmx_graph(request):
     template_name = 'htmx_graph.html'
 
@@ -63,23 +65,26 @@ def htmx_graph(request):
     readings = [float(v.value) if v.value else None for v in values]
 
     initial_data = {
-        "x": timestamps,
-        "y": readings,
-        "xMin": (values[0].time_of_reading - timedelta(hours=2)).isoformat(),
-        "xMax": values[0].time_of_reading.isoformat()
+        'x': timestamps,
+        'y': readings,
+        'xMin': (values[0].time_of_reading - timedelta(hours=2)).isoformat(),
+        'xMax': values[0].time_of_reading.isoformat(),
+        'yRange': [0, 20]
     }
     update_url = "test_url"
     update_interval = 100
 
     return render(request, template_name, {
-        "initial_data": initial_data,
+        "initial_data": json.dumps(initial_data),
         "update_url": update_url,
         "update_interval": update_interval
     })
 
+
 def htmx_base(request):
     template_name = 'htmx_base.html'
     return render(request, template_name)
+
 
 class EntriesView(views.View):
     def get(self, request):
